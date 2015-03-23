7b94212103956480c02c02c4cc5b33f0fdbbdb22
Mikael Bang <mackeb1@gmail.com>
2015-03-23 09:53:52 +0100

    Added some labels to show test data

--- resources/views/stocks/index.php
+++ resources/views/stocks/index.php
@@ -4,13 +4,12 @@
     <link rel="stylesheet" href="resources/css/main.css" type="text/css"/>
 </head>
     <body>
-        <p><?php echo($stocks->first_name . " " . $stocks->last_name) ?> </p>
+        <p>User: <?php echo($stocks->first_name . " " . $stocks->last_name) ?> </p>
         <div>
-            <p><?php echo($stocks->name) ?> </p>
-            <p><?php echo($stocks->cost) ?> </p>
-            <p><?php echo($stocks->info) ?> </p>
-            <p><?php echo($stocks->holding) ?> </p>
-
+            <p>Stock Name : <?php echo($stocks->name) ?> </p>
+            <p>Cost: <?php echo($stocks->cost) ?> </p>
+            <p>Info: <?php echo($stocks->info) ?> </p>
+            <p>Holding: <?php echo($stocks->holding) ?> </p>
         </div>
     </body>
 </html>
